4248fea20fc4b5c88ef76d6585d4a5d56133775c
Akash Kumar <122812784+Akash-Kumar1107@users.noreply.github.com>
2024-06-28 15:58:41 +0530

    vite+react
    Timer: vite+react

---
+++ src/UserData.js
@@ -0,0 +1,26 @@
+
+export const UserData = [
+    {
+        id: 1,
+        FirstName : "Jon",
+        LastName : "Doe",
+        age: 20,
+
+    },
+    {
+        id: 2,
+        FirstName : "Brit",
+        LastName : "Watson",
+        age: 20,
+    },
+
+     {
+        id: 2,
+        FirstName : "Shawn",
+        LastName : "Cook",
+        age: 20,
+    },
+
+
+  
+]
